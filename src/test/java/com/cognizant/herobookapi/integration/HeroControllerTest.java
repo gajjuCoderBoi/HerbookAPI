2bd71b0586eac02a99a69c9530a01b7ef5f34e9e
Mohammad Javed <849897@cognizant.com>
2021-03-15 19:09:05 -0400

    Case #1: Red.

---
+++ src/test/java/com/cognizant/herobookapi/integration/HeroControllerTest.java
@@ -0,0 +1,38 @@
+package com.cognizant.herobookapi.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HeroControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void allHerosTest() throws Exception {
+
+        RequestBuilder rq = get("/hero/list")
+                            .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(rq)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("heros", hasSize(2)))
+                .andDo(print())
+        ;
+
+    }
+
+}
